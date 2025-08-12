d0f9026f02d9b78899c8e94901f5528d09bae690
alaina-furstenberg <alaina.furstenberg@gmail.com>
2025-08-12 11:51:31 -0700

    Update next.config.ts

--- Paste & Post/paste-and-post/next.config.ts
+++ Paste & Post/paste-and-post/next.config.ts
@@ -1,7 +1,10 @@
-import type { NextConfig } from "next";
+ import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+  const nextConfig: NextConfig = {
+    output: 'standalone',
+    experimental: {
+      serverComponentsExternalPackages: ['cheerio']
+    }
+  };
 
-export default nextConfig;
+  export default nextConfig;
